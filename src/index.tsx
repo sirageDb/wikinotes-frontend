b3b44624adce4a7586f20bbd5ce2e5a5d2242cd4
sirageDb <ilovesemicolons@hotmail.com>
2022-05-17 04:25:05 +0200

    ssl

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ import reportWebVitals from './reportWebVitals';
 
 
 // const httpLink = new HttpLink({ uri: process.env.REACT_APP_API_URL });
-const httpLink = new HttpLink({ uri: "https://everco.io:5000" });
+const httpLink = new HttpLink({ uri: "http://everco.io:5000" });
 // const httpLink = new HttpLink({ uri: "http://localhost:5000/" });
 
 const authMidlw = new ApolloLink((operation, next) => {
